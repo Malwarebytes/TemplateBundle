4eaf594596aa4eaf369007f9238b61178cdf2569
Josh Hall-Bachner <jhallbachner@gmail.com>
2013-11-14 17:45:00 -0800

    Initial construction of the template catalog and default controller.

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -5,15 +5,38 @@ namespace Malwarebytes\TemplateBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/templates")
      * @Template()
      */
-    public function indexAction($name)
+    public function listAction()
     {
-        return array('name' => $name);
+        $catalog = $this->get('malwarebytes_template.catalog');
+
+        $tnames = $catalog->getTemplates();
+
+        $templates = array();
+        foreach($tnames as $tname) {
+            $route = $this->get('router')->generate('malwarebytes_template_default_template',
+                array('template' => $tname)
+            );
+            $templates[] = "<a href='$route'>$tname</a><br>";
+        }
+
+        return array('templates' => $templates, 'topline' => 'Templates', 'title' => 'Templates');
+    }
+
+    /**
+     * @Route("/template/{template}", requirements={"template" = ".+"})
+     */
+    public function templateAction($template)
+    {
+        $twig = $this->get('twig');
+
+        return new Response($twig->render($template));
     }
 }
---
+++ Service/TemplateCatalog.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Malwarebytes\TemplateBundle\Service;
+
+use Symfony\Component\Finder\Finder;
+use Twig_Environment,
+    Twig_Loader_Filesystem;
+
+class TemplateCatalog
+{
+    protected $twig;
+
+    public function __construct(Twig_Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    public function getTemplates()
+    {
+        $loader = $this->twig->getLoader();
+
+        if(!$loader instanceof Twig_Loader_Filesystem) {
+            return false;
+        }
+
+        $namespaces = $loader->getNamespaces();
+        $templates = array();
+        foreach($namespaces as $namespace) {
+            $paths = $loader->getPaths($namespace);
+
+            foreach($paths as $path) {
+                $finder = new Finder();
+                $finder->in($path)->name('*.*.twig');
+                foreach($finder as $file) {
+                    $localpath = substr($file->getPath(), strlen($path));
+                    $templatename = "@$namespace$localpath/" . $file->getFilename();
+
+                    $templates[] = $templatename;
+                }
+            }
+        }
+
+        return $templates;
+    }
+}
