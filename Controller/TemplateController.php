1d533126a7364268bcb4ab502dfc90ea0cc8a5c7
Josh Hall-Bachner <jhallbachner@gmail.com>
2013-12-04 11:10:25 -0800

    Updated a wide range of stuff in pursuit of a first beta version of functionality.

--- Controller/TemplateController.php
+++ Controller/TemplateController.php
@@ -5,6 +5,7 @@ namespace Malwarebytes\TemplateBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class TemplateController extends Controller
@@ -34,15 +35,22 @@ class TemplateController extends Controller
      * @Route("template/info/{template}", requirements={"template" = ".+"})
      * @Template()
      */
-    public function infoAction($template)
+    public function infoAction($template, Request $request)
     {
         $catalog = $this->get('malwarebytes_template.catalog');
 
         $info = $catalog->getInfo($template);
 
-        $form = $this->createForm('template', array('template' => $template));
+        $form = $this->createForm('template', array('__template' => $template));
 
-        return array('template' => $info, 'topline' => "Info: $template", 'title' => "Template Info: $template", 'form' => $form->createView());
+        $form->handleRequest($request);
+
+        if($form->isValid()) {
+            $data = $this->turnFormDataIntoTemplateData($form->getData());
+            return $this->render($template, $data);
+        } else {
+            return array('template' => $info, 'topline' => "Info: $template", 'title' => "Template Info: $template", 'form' => $form->createView());
+        }
     }
 
     /**
@@ -53,5 +61,43 @@ class TemplateController extends Controller
         $twig = $this->get('twig');
 
         return new Response($twig->render($template));
+    }
+
+    protected function turnFormDataIntoTemplateData($data)
+    {
+        $fields = array();
+
+        foreach($data as $name => $value) {
+            if($name === '__template') {
+                continue;
+            }
+
+            $steps = explode(':', $name);
+            $point = &$fields;
+
+            while(($item = array_shift($steps)) !== null) {
+                $member = (strpos($item, '---') === false) ? false : true;
+                if($member) {
+                    $item = '---';
+                }
+
+                if(count($steps) == 0) {
+                    if($member) {
+                        $point[0] = $value;
+                        $point[1] = $value;
+                        $point[2] = $value;
+                    } else {
+                        $point[$item] = $value;
+                    }
+                } elseif(isset($point[$item]) && is_array($point[$item])) {
+                    $point = &$point[$item];
+                } elseif(!isset($point[$item])) {
+                    $point[$item] = array();
+                    $point = &$point[$item];
+                }
+            }
+        }
+var_dump($fields);
+        return $fields;
     }
 }
--- Form/TemplateDataFormType.php
+++ Form/TemplateDataFormType.php
@@ -20,13 +20,51 @@ class TemplateDataFormType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event) {
+        $build = function($tree, $prefix = '') use (&$build) {
+            $fields = array();
+
+            foreach(array('expressions', 'members', 'items') as $sub) {
+
+                if(isset($tree[$sub])) {
+                    foreach($tree[$sub] as $name => $value) {
+                        if(count($value) == 0) {
+                            $n = $prefix . $name;
+                            if($sub === 'items') {
+                                $n .= '---';
+                            }
+                            $fields[] = $n;
+                        } else {
+                            $delim = ($sub == 'items') ? '---:' : ':';
+                            if($prefix !== '') {
+                                $p = "{$prefix}{$name}{$delim}";
+                            } else {
+                                $p = "{$name}{$delim}";
+                            }
+                            $fields = array_merge($fields, $build($value, $p));
+                        }
+                    }
+                }
+            }
+
+            return $fields;
+        };
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event) use ($build) {
             $data = $event->getData();
             $form = $event->getForm();
 
-            $form->add('template', 'text', array('label' => 'Template Name', 'data' => $data['template']));
+            $form->add('__template', 'hidden', array('label' => 'Template Name', 'data' => $data['__template']));
 
-            $info = $this->catalog->getInfo($data['template']);
+            $info = $this->catalog->getInfo($data['__template']);
+
+            $fields = $build($info);
+
+            foreach($fields as $field) {
+                $form->add($field, 'text', array('label' => $field));
+            }
+
+            $form->add('render', 'submit', array('label' => 'Render Template'));
+
         });
     }
 
