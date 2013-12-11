8eb11beb82846f4f7232a118a9dc2e0ebfa7e8f2
Josh Hall-Bachner <jhallbachner@gmail.com>
2013-12-11 10:38:02 -0800

    Began expanding out to parse comments.

---
+++ Twig/MetadataTokenParser.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Malwarebytes\TemplateBundle\Twig;
+
+use Twig_Error_Syntax;
+use Twig_NodeInterface;
+use Twig_Token;
+use Twig_TokenParser;
+use Twig_TokenParserInterface;
+
+class MetadataTokenParser extends Twig_TokenParser implements Twig_TokenParserInterface
+{
+
+    public function parse(Twig_Token $token)
+    {
+        // TODO: Implement parse() method.
+    }
+
+    public function getTag()
+    {
+        // TODO: Implement getTag() method.
+    }
+}
